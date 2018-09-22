d2c430b8825eaef4e3b48c72bee34a0f8fd2b2be
KazuruK <esinnikita.2000@mail.ru>
2018-09-22 16:58:54 +0300

    Сделал Lesson 1 и Lesson 2 (иправления) 3

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -67,10 +67,9 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
 fun ageDescription(age: Int): String {
     return when {
         age % 10 > 4 -> "$age лет"
-        age in 10..20 -> "$age лет"
-        age in 110..120 -> "$age лет"
         age % 10 == 1 -> "$age год"
         age % 100 in 10..20 -> "$age лет"
+        age % 10 == 0 -> "$age лет"
         else -> "$age года"
 
     }
@@ -152,7 +151,7 @@ fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
     return when {
         ((a < c) && (b < c)) || ((a > c) && (a > d)) -> -1
         (a < c) && (c <= b) && (b <= d) -> cb
-        (a >= c) && (c <= b) && (b < d) -> ab
+        (a >= c) && (c <= b) && (b <= d) -> ab
         (a > c) && (c < b) && (b > d) && (a <= d) -> ad
         else -> cd
     }
